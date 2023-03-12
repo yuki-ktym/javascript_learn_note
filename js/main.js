ce297364d8ba5e9f81346a9e00664c501a658ac5
yuki_ktym <sins.kita@gmail.com>
2023-03-12 13:56:32 +0900

    add file "html","js"

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// コメントアウト
+
+// ログの表出、現在時刻の取得、テキストの表出
+console.log("hellow", "it's", new Date(), "now")
+
+// 四則演算
+console.log(100 + 200)
+console.log(200 - 80)
+console.log(100 * 3)
+console.log(400 / 5)
+console.log(402 % 5)
+console.log(2 ** 3)
+
+// 変数
+let myName = "kitayama"
+console.log(myName)
